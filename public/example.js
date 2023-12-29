2cafd5748ebe7054a8bdbc9495c2f2eed7089fe3
Crllect <144494583+crllect@users.noreply.github.com>
2023-12-29 15:23:17 -0500

    add template

---
+++ public/example.js
@@ -0,0 +1,25 @@
+document // makes it so you can press enter to submit as opposed to just being able to press a button
+    .getElementById("urlInput")
+    .addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            document.getElementById("searchButton").click();
+        }
+    });
+
+document.getElementById("searchButton").onclick = function (event) {
+    event.preventDefault();
+
+    let url = document.getElementById("urlInput").value; // If no periods are detected in the input, search google instead
+    let searchUrl = "https://www.google.com/search?q=";
+
+    if (!url.includes(".")) {
+        url = searchUrl + encodeURIComponent(url);
+    } else {
+        if (!url.startsWith("http://") && !url.startsWith("https://")) { // if no http or https is detected, add https automatically
+            url = "https://" + url;
+        }
+    }
+
+    iframeWindow.src = __uv$config.prefix + __uv$config.encodeUrl(url);
+};
---
+++ server.js
@@ -0,0 +1,64 @@
+import { createBareServer } from "@tomphttp/bare-server-node";
+import express from "express";
+import { createServer } from "node:http";
+import { join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = join(fileURLToPath(import.meta.url), "..");
+const bare = createBareServer("/bare/");
+const app = express();
+const publicPath = "public"; // if you renamed your directory to something else other than public
+
+app.use(express.static(publicPath));
+
+app.use((req, res) => {
+    res.status(404);
+    res.sendFile(join(__dirname, publicPath, "404.html")); // change to your 404 page
+});
+
+const server = createServer();
+
+server.on("request", (req, res) => {
+    if (bare.shouldRoute(req)) {
+        bare.routeRequest(req, res);
+    } else {
+        app(req, res);
+    }
+});
+
+server.on("upgrade", (req, socket, head) => {
+    if (bare.shouldRoute(req)) {
+        bare.routeUpgrade(req, socket, head);
+    } else {
+        socket.end();
+    }
+});
+
+let port = parseInt(process.env.PORT || "");
+
+if (isNaN(port)) port = 8080; // set your port
+
+server.on("listening", () => {
+    const address = server.address();
+    console.log("Listening on:");
+    console.log(`\thttp://localhost:${address.port}`);
+    console.log(
+        `\thttp://${
+            address.family === "IPv6" ? `[${address.address}]` : address.address
+        }:${address.port}`
+    );
+});
+
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
+
+function shutdown() {
+    console.log("SIGTERM signal received: closing HTTP server");
+    server.close();
+    bare.close();
+    process.exit(0);
+}
+
+server.listen({
+    port,
+});
