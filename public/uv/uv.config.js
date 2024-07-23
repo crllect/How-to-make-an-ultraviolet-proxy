9a1c38636c7ac4dbea39aa0f171332f9793a2a82
Percs <83934299+Percslol@users.noreply.github.com>
2024-07-23 06:11:21 -0500

    make some slight changes

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,7 +1,7 @@
 // This file overwrites the stock UV config.js
 
 self.__uv$config = {
-    prefix: "/service/",
+    prefix: "/uv/service/",
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: "/uv/uv.handler.js",
